1668d63ef0351f62320d4f1b8bbf88bc5142b683
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-06-07 20:55:34 +0300

    create the homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import AuthProvider from 'auth/AuthProvider';
+import { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import AuthProvider from 'auth/AuthProvider';
+import Loading from 'components/common/loading/Loading';
 import Routes from 'routes/Routes';
 import CustomThemeProvider from 'theme/CustomThemeProvider';
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <CustomThemeProvider>
-          <Routes />
-        </CustomThemeProvider>
-      </AuthProvider>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <AuthProvider>
+          <CustomThemeProvider>
+            <Routes />
+          </CustomThemeProvider>
+        </AuthProvider>
+      </Router>
+    </Suspense>
   );
 }
 
