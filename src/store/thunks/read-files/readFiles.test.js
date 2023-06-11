9ddfda190dae3fcb1c9119fc23dcbc6cacca853b
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-06-11 14:42:54 +0300

    Added tests for read files thunk

--- src/store/thunks/read-files/readFiles.test.js
+++ src/store/thunks/read-files/readFiles.test.js
@@ -37,4 +37,31 @@ describe('readFiles', () => {
     expect(dispatch.mock.calls[1]).toEqual([{ type: 'files/setFiles', payload: userFiles }]);
     expect(dispatch.mock.calls[2][0].type).toEqual('files/readFiles/fulfilled');
   });
+
+  it('shouldnt read two files', async () => {
+    const userFiles = [
+      {
+        id: '1',
+        name: 'index.js',
+        code: 'console.log("hello world")',
+        extension: 'js',
+        relativePath: 'src/index.js',
+      },
+      {
+        id: '2',
+        name: 'main.js',
+        code: 'console.log("hello world")',
+        extension: 'js',
+        relativePath: 'src/main.js',
+      },
+    ];
+    readFile.mockReturnValueOnce(userFiles[0]);
+    readFile.mockReturnValueOnce(userFiles[1]);
+    await readFiles([1, 3, 8, 6])(dispatch, getState);
+    expect(dispatch).toHaveBeenCalledTimes(3);
+    expect(dispatch.mock.calls[0][0].type).toEqual('files/readFiles/pending');
+    console.log(dispatch.mock.calls);
+    expect(dispatch.mock.calls[1]).toEqual([{ type: 'files/setFiles', payload: [...userFiles, undefined, undefined] }]);
+    expect(dispatch.mock.calls[2][0].type).toEqual('files/readFiles/fulfilled');
+  });
 });
--- src/store/thunks/read-files/readFiles.ts
+++ src/store/thunks/read-files/readFiles.ts
@@ -1,15 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
 import { setFiles } from 'store/slices/files/files';
 import { readFile } from 'utils/read-file';
 
 export const readFiles = createAsyncThunk('files/readFiles', async (files: FileList, { dispatch }) => {
-  const promises = [];
+  try {
+    const promises = [];
+    console.log(files, 'files');
 
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    promises.push(readFile(file));
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      promises.push(readFile(file));
+    }
+
+    const userFiles = await Promise.all(promises);
+    dispatch(setFiles(userFiles));
+  } catch (error) {
+    console.log(error);
   }
-
-  const userFiles = await Promise.all(promises);
-  dispatch(setFiles(userFiles));
 });
