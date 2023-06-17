92b23ef74c1f3a3cd4a8e8133c0710cccc5667d9
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-06-17 12:57:38 +0300

    Added open workspace button

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import paths from 'routes/paths';
 import CodeEditorButton from './CodeEditorButton';
 import DarkModeSwitch from './DarkModeSwitch';
 import SignInButton from './SignInButton';
 import SignOutButton from './SignOutButton';
+import OpenWorkspaceButton from './OpenWorkspaceButton';
 
 const StyledLink = styled(Link)(({ theme }) => ({
   textDecoration: 'none',
@@ -37,12 +38,16 @@ const UnauthenticatedButtons = () => {
   );
 };
 
+const AuthenticatedButtonsContainer = styled('div')({ display: 'flex' });
+
 const AuthenticatedButtons = () => {
+  const history = useHistory();
+
   return (
-    <div>
-      <CodeEditorButton />
+    <AuthenticatedButtonsContainer>
+      {history.location.pathname === paths.home ? <CodeEditorButton /> : <OpenWorkspaceButton />}
       <SignOutButton />
-    </div>
+    </AuthenticatedButtonsContainer>
   );
 };
 
